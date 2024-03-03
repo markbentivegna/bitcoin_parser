206a5483898ed73835fece653cf2e83b637c4682
Mark Bentivegna <markbentivegna@gmail.com>
2024-03-03 10:28:39 -0500

    new message

---
+++ Block.py
@@ -0,0 +1,65 @@
+from block_util import *
+from Transaction import Transaction
+from BlockHeader import BlockHeader
+
+class Block:
+    def __init__(self, blockchain):
+        self.continue_parsing = True
+        self.magic_number = 0
+        self.block_size = 0
+        self.block_header = ""
+        self.tx_count = 0
+        self.tx_list = []
+
+        if self.has_length(blockchain, 8):
+            self.magic_number = uint4(blockchain)
+            self.block_size = uint4(blockchain)
+        else:
+            self.continue_parsing = False
+            return
+        
+        if self.has_length(blockchain, self.block_size):
+            self.set_header(blockchain)
+            self.tx_count = varint(blockchain)
+            self.tx_list = []
+
+            for i in range(self.tx_count):
+                tx = Transaction(blockchain)
+                tx.seq = i
+                self.tx_list.append(tx)
+        else:
+            self.continue_parsing = False
+
+    def continue_parsing(self):
+        return self.continue_parsing
+    
+    def get_block_size(self):
+        return self.block_size
+    
+    def has_length(self, blockchain, size):
+        current_position = blockchain.tell()
+        blockchain.seek(0,2)
+
+        file_size = blockchain.tell()
+        blockchain.seek(current_position)
+
+        temp_block_size = file_size - current_position
+        print(f"temp_block_size:\t {temp_block_size}")
+        if temp_block_size < size:
+            return False
+        return True
+    
+    def set_header(self, blockchain):
+        self.block_header = BlockHeader(blockchain)
+
+    def to_string(self):
+        print(f"")
+        print(f"Magic Number:\t {self.magic_number}")
+        print(f"Block Size:\t {self.block_size}")
+        print(f"")
+        print(f"{'#'*10} Block Header {'#'*10}")
+        print(f"{self.block_header.to_string()}")
+        print(f"{'#' * 5} Tx count: {self.tx_count}")
+        for tx in self.tx_list:
+            print(f"{tx.to_string()}")
+        print(f"{'#' * 5} end of all transactions: {self.tx_count}")
---
+++ BlockHeader.py
@@ -0,0 +1,23 @@
+from block_util import *
+from datetime import datetime
+
+class BlockHeader:
+    def __init__(self, blockchain):
+        self.version = uint4(blockchain)
+        self.previous_hash = hash32(blockchain)
+        self.merkle_hash = hash32(blockchain)
+        self.time = uint4(blockchain)
+        self.bits = uint4(blockchain)
+        self.nonce = uint4(blockchain)
+
+    def to_string(self):
+        print(f"Version:\t {self.version}")
+        print(f"Previous Hash:\t {hash_string(self.previous_hash)}")
+        print(f"Merkle Root\t: {hash_string(self.merkle_hash)}")
+        print(f"Timestamp:\t {self.decode_time(self.time)}")
+        print(f"Difficulty:\t {self.bits}")
+        print(f"Nonce:\t\t {self.nonce}")
+
+    def decode_time(self, time):
+        utc_time = datetime.utcfromtimestamp(time)
+        return utc_time.strftime("%Y-%m-%d %H:%M:%S.%f+00:00 (UTC)")
